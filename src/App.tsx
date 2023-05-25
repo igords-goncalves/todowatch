e9875c2e1aab6aa1757ce183fd94cedf490c4388
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-25 19:56:09 -0300

    refactor: change components parent to ease prop drilling

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import './App.sass';
-import { Form } from './components/Form';
 import { Timer } from './components/Timer';
-import { List } from './components/List/index';
 
 function App() {
-  return (
-    <>
-      <div className="card">
-        <Timer />
-        <Form label="Task" placeHolder="Enter your task here ..." />
-        <List />
-      </div>
-    </>
-  );
+  return <Timer />;
 }
 
 export default App;
