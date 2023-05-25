d91b88344d23a75250527d462c3fb967879299e2
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-25 19:56:53 -0300

    refactor: refactor component timer

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,17 +1,48 @@
 import styles from './Timer.module.sass';
+import { useState, useEffect } from 'react';
+import { insertZero } from './utils/insertZero';
+import { handleTimer } from './utils/handleTimer';
+import { startStopTimer } from './utils/startStopTimer';
+import { Form } from '../Form';
+import { List } from '../List';
+import play from '../../assets/play.svg';
+import pause from '../../assets/pause.svg';
+import { Button } from '../Button';
 
-interface TimerProps {
-  hours: string;
-  minutes: string;
-  seconds: string;
-}
+export const Timer: React.FC = () => {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(59);
+  const [seconds, setSeconds] = useState(50);
+  const [isRunning, setIsRunning] = useState(false);
+  const [label, setLabel] = useState(play);
 
-export const Timer: React.FC<TimerProps> = ({ hours, minutes, seconds }) => {
+  useEffect(
+    () => startStopTimer({ setSeconds, setMinutes, setHours }, isRunning),
+    [isRunning],
+  );
+
   return (
-    <div className={styles['c-timer']}>
-      <span className={styles['c-timer__hours']}>{hours}:</span>
-      <span className={styles['c-timer__minutes']}>{minutes}:</span>
-      <span className={styles['c-timer__seconds']}>{seconds}</span>
-    </div>
+    <>
+      <div className={styles['c-timer']}>
+        <span className={styles['c-timer__hours']}>{insertZero(hours)}:</span>
+        <span className={styles['c-timer__minutes']}>
+          {insertZero(minutes)}:
+        </span>
+        <span className={styles['c-timer__seconds']}>
+          {insertZero(seconds)}
+        </span>
+      </div>
+      <div className="card">
+        <Form label="Task" placeHolder="Enter your task here ..." />
+        <Button
+          label={label}
+          onClick={() => {
+            isRunning ? setLabel(play) : setLabel(pause);
+            handleTimer(setIsRunning);
+          }}
+        />
+        <List />
+      </div>
+    </>
   );
 };
