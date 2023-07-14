698c773e3db4e12fe0e6ea0f3e8cd9a9187bc40b
Igor Gonçalves <igordossantos38@gmail.com>
2023-07-14 20:01:39 -0300

    feat: insert alt and adjust acessibility

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,14 +2,15 @@ import styles from './Button.module.sass';
 
 interface ButtonProps {
   label: string;
+  alt: string;
   onClick: () => void;
   children?: React.ReactNode;
 }
 
-export const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+export const Button: React.FC<ButtonProps> = ({ label, onClick, alt }) => {
   return (
-    <button className={styles['c-button']} onClick={onClick}>
-      <img src={label} />
+    <button className={styles['c-button']} onClick={onClick} aria-label="Botão">
+      <img src={label} alt={alt} />
     </button>
   );
 };
