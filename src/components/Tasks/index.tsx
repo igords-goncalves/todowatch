909fd6649a1d28a7e359a143a68980d8d90718e3
Igor Gonçalves <igordossantos38@gmail.com>
2023-07-14 20:02:57 -0300

    feat: adjusment in table and texts

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -1,6 +1,7 @@
 import styles from './Tasks.module.sass';
 import trash from '../../assets/trash.svg';
 import edit from '../../assets/edit.svg';
+import Balancer from 'react-wrap-balancer';
 
 interface TasksProps {
   time: string;
@@ -9,16 +10,27 @@ interface TasksProps {
 
 export const Tasks: React.FC<TasksProps> = ({ time, task }) => {
   return (
-    <li className={styles['c-tasks__item']}>
-      <div>{time}</div> <div>{task}</div>
-      <div className={styles['c-tasks__btn']}>
-        <button onClick={() => console.log('edit')}>
-          <img src={edit} />
-        </button>
-        <button onClick={() => console.log('delete')}>
-          <img src={trash} />
-        </button>
-      </div>
-    </li>
+    <tbody className={styles['c-tasks__item']}>
+      <tr>
+        <td>
+          <div>{time}</div>
+        </td>
+        <td>
+          <div>
+            <Balancer>{task}</Balancer>
+          </div>
+        </td>
+        <td>
+          <div className={styles['c-tasks__btn']}>
+            <button onClick={() => console.log('edit')} aria-label="Botão">
+              <img src={edit} alt="Botão editar" />
+            </button>
+            <button onClick={() => console.log('delete')} aria-label="Botão">
+              <img src={trash} alt="Botão deletar" />
+            </button>
+          </div>
+        </td>
+      </tr>
+    </tbody>
   );
 };
